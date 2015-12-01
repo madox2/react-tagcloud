cc35edfca46d029415a237241b01152e6f50ed4f
Martin Bielik <mx.bielik@gmail.com>
2015-12-01 21:30:24 +0100

    initial version

---
+++ examples/index.js
@@ -0,0 +1,44 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import {TagCloud, DefaultRenderer} from "../lib/index";
+
+const tags = [
+    {
+        value: "jQuery",
+        count: 2
+    },
+    {
+        value: "SQL",
+        count: 2
+    },
+    {
+        value: "JavaScript",
+        count: 4
+    },
+    {
+        value: "React",
+        count: 3
+    },
+    {
+        value: "Nodejs",
+        count: 4
+    },
+    {
+        value: "Express.js",
+        count: 2
+    },
+    {
+        value: "HTML5",
+        count: 1
+    }
+]
+
+const renderer = DefaultRenderer({
+    props: {
+        onClick: () => console.log("Hello word")
+    }
+})
+
+ReactDOM.render(
+    <TagCloud levels={5} tags={tags} style={{width: 300}} renderer={renderer} />, document.getElementById("examples")
+);
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+
+module.exports = {
+    context: __dirname + "/examples",
+    entry: {
+        javascript: "./index.js",
+        html: "./index.html"
+    },
+    output: {
+        filename: "bundle.js",
+        path: __dirname + "/dist",
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loaders: ["react-hot", "babel-loader"],
+            },
+            {
+                test: /\.html$/,
+                loaders: ["react-hot", "file?name=[name].[ext]"],
+            }
+        ]
+    }
+}
