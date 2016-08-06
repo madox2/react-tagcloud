5d564694ca0ba6a2f621cf56617d798ff8bb491b
Martin Bielik <mx.bielik@gmail.com>
2016-08-06 14:06:45 +0200

    refactored index page

--- examples/index.js
+++ examples/index.js
@@ -7,10 +7,25 @@ import DefaultRendererExample from "./default-renderer";
 import CustomTagRendererExample from "./custom-tag-renderer";
 import CustomRendererExample from "./custom-renderer";
 
-const el = document.getElementById.bind(document);
+const App = () => (
+  <div>
+    <h1>TagCloud examples</h1>
 
-ReactDOM.render(<SimpleCloudExample />, el("simple-cloud"));
-ReactDOM.render(<CustomPropsExample />, el("custom-props"));
-ReactDOM.render(<DefaultRendererExample />, el("default-renderer"));
-ReactDOM.render(<CustomTagRendererExample />, el("custom-tag-renderer"));
-ReactDOM.render(<CustomRendererExample />, el("custom-renderer"));
+    <h2>Simple cloud</h2>
+    <SimpleCloudExample />
+
+    <h2>Custom props</h2>
+    <CustomPropsExample />
+
+    <h2>Default renderer</h2>
+    <DefaultRendererExample />
+
+    <h2>Custom tag renderer</h2>
+    <CustomTagRendererExample />
+
+    <h2>Custom renderer</h2>
+    <CustomRendererExample />
+  </div>
+);
+
+ReactDOM.render(<App />, document.getElementById("app"));
