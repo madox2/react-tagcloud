62defa275deda52bf565f0f99a38d309cb74caec
Martin Bielik <mx.bielik@gmail.com>
2016-08-08 20:31:19 +0200

    fetch polyfill

--- examples/index.js
+++ examples/index.js
@@ -2,8 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Highlight from "react-highlight";
 
-require('./index.html');
-require('./css/styles.css');
+import 'whatwg-fetch';
+import './index.html';
+import './css/styles.css';
 
 const BASE_URL = 'https://raw.githubusercontent.com/madox2/react-tagcloud/master/examples/';
 
