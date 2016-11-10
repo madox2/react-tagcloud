47f4ca9f63b25c02541c650cbf42030b90a2585a
Martin Bielik <martin.bielik@outlook.sk>
2016-11-10 18:18:27 +0100

    added example data

--- examples/src/custom-renderer.js
+++ examples/src/custom-renderer.js
@@ -5,7 +5,12 @@ const data = [
   { value: "jQuery", count: 25 }, { value: "MongoDB", count: 18 },
   { value: "JavaScript", count: 38 }, { value: "React", count: 30 },
   { value: "Nodejs", count: 28 }, { value: "Express.js", count: 25 },
-  { value: "HTML5", count: 33 }, { value: "CSS3", count: 20 }
+  { value: "HTML5", count: 33 }, { value: "CSS3", count: 20 },
+  { value: "Webpack", count: 22 }, { value: "Babel.js", count: 7 },
+  { value: "ECMAScript", count: 25 }, { value: "Jest", count: 15 },
+  { value: "Mocha", count: 17 }, { value: "React Native", count: 27 },
+  { value: "Angular.js", count: 30 }, { value: "TypeScript", count: 15 },
+  { value: "Flow", count: 30 }, { value: "NPM", count: 11 },
 ];
 
 // custom renderer is function which has tag, computed font size and
