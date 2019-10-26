00acbd0f86ba6258ff6188a35ecc031374d4ea6a
Martin Bielik <mx.bielik@gmail.com>
2019-10-26 12:54:13 +0200

    v2

---
+++ precache-manifest.2a11b041497169999849e6ec4f75f767.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d848944d1d2b24ba5bbe8423ecac59ef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ef15e67de828f8bd0854",
+    "url": "/static/css/main.e41f96df.chunk.css"
+  },
+  {
+    "revision": "807e671405bfd5e38045",
+    "url": "/static/js/2.6fbea8a9.chunk.js"
+  },
+  {
+    "revision": "ef15e67de828f8bd0854",
+    "url": "/static/js/main.1fa482ca.chunk.js"
+  },
+  {
+    "revision": "820b252efc1a44843c42",
+    "url": "/static/js/runtime-main.bbc8ffcf.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
