ed96925af37732975e206cbbfffc31437dc71c85
Martin Bielik <mx.bielik@gmail.com>
2019-10-26 12:22:21 +0200

    removed unneded polyfill

--- src/defaultRenderer.js
+++ src/defaultRenderer.js
@@ -1,10 +1,9 @@
 import React from 'react'
-import objectAssign from 'object-assign'
 
 export const defaultRenderer = (tag, size, color) => {
   const fontSize = size + 'px'
   const key = tag.key || tag.value
-  const style = objectAssign({}, styles, { color, fontSize })
+  const style = { ...styles, color, fontSize }
   return (
     <span className="tag-cloud-tag" style={style} key={key}>
       {tag.value}
