0080ba42228e867fe1804c822971fe555dd8e625
Martin Bielik <mx.bielik@gmail.com>
2021-12-28 15:46:42 +0100

    dark code highlighting

--- examples/src/index.js
+++ examples/src/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import './styles/index.css'
-import './styles/github-gist.css'
+import '../node_modules/highlight.js/styles/atom-one-dark.css'
 import 'whatwg-fetch'
 
 ReactDOM.render(<App />, document.getElementById('root'))
